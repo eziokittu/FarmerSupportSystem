f7bd6d4a5a4c8efb1bb50835de7b8beb92832a62
Bodhisatta Bhattacharjee <bodhikittu@gmail.com>
2024-11-20 05:56:05 +0530

    updated server

--- server/app.py
+++ server/app.py
@@ -7,6 +7,8 @@ import io
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+PORT = 5000
+
 # Load the existing Hugging Face model
 pipe = pipeline("image-classification", model="wambugu71/crop_leaf_diseases_vit")
 # pipe2 = pipeline("image-classification", model="Diginsa/Plant-Disease-Detection-Project")
@@ -24,6 +26,19 @@ def classify_image():
     predictions = pipe(image)
 
     return jsonify(predictions)
+  
+# dummy route to check if server is active
+@app.route('/api/working', methods=['GET'])
+def workingStatus():
+    try:
+        return jsonify({'ok': 1, 'message': 'server is active'}), 200
+    except Exception as e:
+        return jsonify({'ok': -1, 'message': 'server still not active!'}), 500
+    
+# default route
+@app.route("/")
+def home():
+    return f"Running Flask with sentiment analysis! PORT: {PORT}!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
